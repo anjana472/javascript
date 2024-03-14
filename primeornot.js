4111fd7d7dbd9196f3e938dd6547f59e7323f03a
ANJANA P <110957441+anjana472@users.noreply.github.com>
2024-03-14 10:16:00 +0530

    Add files via upload

---
+++ primeornot.js
@@ -0,0 +1,17 @@
+function isPrime (number) {
+    if(number <= 1) {
+
+        return false;
+    }
+
+for(i=2;i<=Math.sqrt(number);i++) {
+    if (number%i==0) {
+        return false;
+    }
+}
+
+return true;
+
+}
+
+console.log(isPrime(17));
