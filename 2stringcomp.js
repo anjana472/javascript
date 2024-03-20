69c6aa885b4015bf2fc294f3c55ec1707fefbac1
ANJANA P <110957441+anjana472@users.noreply.github.com>
2024-03-20 10:58:00 +0530

    Add files via upload

---
+++ 2stringcomp.js
@@ -0,0 +1,14 @@
+function compareStrings(string1,string2){
+    if(string1==string2)
+    {
+        return "The strings are equal";
+    }
+        else
+            {
+            return "The strings are not equal";
+            }
+    }
+let string1="hellO";
+let string2="hello";
+
+console.log(compareStrings(string1,string2));
